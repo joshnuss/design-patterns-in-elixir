25fe5f07db0ca37c658a8151ff886b004f084b10
Josh Nussbaum <joshnuss@gmail.com>
2015-09-24 22:13:23 -0400

    Refactor strategy pattern

--- strategy/using_fn/run.exs
+++ strategy/using_fn/run.exs
@@ -1,34 +1,33 @@
 # start server with default formatter
-html_formatter = fn context ->
-                  IO.puts "<html>"
-                  IO.puts "  <head>"
-                  IO.puts "    <title>#{context.title}</title>"
-                  IO.puts "  </head>"
-                  IO.puts "  <body>"
+html = fn context ->
+  IO.puts "<html>"
+  IO.puts "  <head>"
+  IO.puts "    <title>#{context.title}</title>"
+  IO.puts "  </head>"
+  IO.puts "  <body>"
 
-                  Enum.each context.text, fn line ->
-                    IO.puts "    <p>#{line}</p>"
-                  end
+  Enum.each context.text, fn line ->
+    IO.puts "    <p>#{line}</p>"
+  end
 
-                  IO.puts "  </body>"
-                  IO.puts "</html>"
-                end
+  IO.puts "  </body>"
+  IO.puts "</html>"
+end
 
-plain_text_formatter = fn context ->
-                         IO.puts "***** #{context.title} *****"
-
-                         Enum.each(context.text, &IO.puts/1)
-                       end
+plain_text = fn context ->
+  IO.puts "***** #{context.title} *****"
+  Enum.each(context.text, &IO.puts/1)
+end
 
 context = %{
   title: "Monthly Report",
   text: ["Things are going", "really, really well"]
 }
 
-{:ok, _} = Report.start_link(html_formatter)
+{:ok, _} = Report.start_link(html)
 
 Report.output_report(context)
 
 # Change the formatter at runtime
-Report.change_format(plain_text_formatter)
+Report.change_format(plain_text)
 Report.output_report(context)
