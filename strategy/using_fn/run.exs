f2b01f0e0c2e3dcd0cf6c4f6198e984238bda83d
Josh Nussbaum <joshnuss@gmail.com>
2015-09-24 22:11:44 -0400

    Refactor strategy pattern when using functions

--- strategy/using_fn/run.exs
+++ strategy/using_fn/run.exs
@@ -1,31 +1,34 @@
 # start server with default formatter
-{:ok, _} = Report.start_link(fn context ->
-    IO.puts "<html>"
-    IO.puts "  <head>"
-    IO.puts "    <title>#{context.title}</title>"
-    IO.puts "  </head>"
-    IO.puts "  <body>"
+html_formatter = fn context ->
+                  IO.puts "<html>"
+                  IO.puts "  <head>"
+                  IO.puts "    <title>#{context.title}</title>"
+                  IO.puts "  </head>"
+                  IO.puts "  <body>"
 
-    Enum.each context.text, fn line ->
-      IO.puts "    <p>#{line}</p>"
-    end
+                  Enum.each context.text, fn line ->
+                    IO.puts "    <p>#{line}</p>"
+                  end
 
-    IO.puts "  </body>"
-    IO.puts "</html>"
-  end)
+                  IO.puts "  </body>"
+                  IO.puts "</html>"
+                end
+
+plain_text_formatter = fn context ->
+                         IO.puts "***** #{context.title} *****"
+
+                         Enum.each(context.text, &IO.puts/1)
+                       end
 
 context = %{
   title: "Monthly Report",
   text: ["Things are going", "really, really well"]
 }
 
+{:ok, _} = Report.start_link(html_formatter)
+
 Report.output_report(context)
 
 # Change the formatter at runtime
-Report.change_format(fn context ->
-  IO.puts "***** #{context.title} *****"
-
-  Enum.each(context.text, &IO.puts/1)
-end)
-
+Report.change_format(plain_text_formatter)
 Report.output_report(context)
