1e2524e37030bc69dcc15f753c9afcf9bc44f4a6
Josh Nussbaum <joshnuss@gmail.com>
2017-02-04 19:44:48 -0500

    Added singleton

---
+++ singleton/run.exs
@@ -0,0 +1,27 @@
+defmodule Singleton do
+  use GenServer
+
+  @name :the_singleton_name
+  @initial_value "starting value"
+
+  def start_link,
+    do: GenServer.start_link(__MODULE__, @initial_value, name: @name)
+
+  def value,
+    do: GenServer.call(@name, :read)
+
+  def update(value),
+    do: GenServer.call(@name, {:write, value})
+
+  def handle_call(:read, _from, value),
+    do: {:reply, value, value}
+
+  def handle_call({:write, value}, _from, _old_value),
+    do: {:reply, :ok, value}
+end
+
+Singleton.start_link
+
+Singleton.value |> IO.puts
+Singleton.update("new value")
+Singleton.value |> IO.puts
