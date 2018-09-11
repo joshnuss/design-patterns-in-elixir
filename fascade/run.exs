4a909c943d39d794a98b7172c514ba6ac0fa04af
Josh Nussbaum <joshnuss@gmail.com>
2018-09-11 17:06:50 -0400

    Adjust docs for fascade

--- fascade/run.exs
+++ fascade/run.exs
@@ -12,11 +12,12 @@ defmodule MySystem.ComponentB do
   end
 end
 
-# Fascade delegate to underlying systems
+# Fascade delegate to underlying components
 defmodule MySystem do
   defdelegate a, to: MySystem.ComponentA
   defdelegate b, to: MySystem.ComponentB
 end
 
+# Call fascade
 MySystem.a()
 MySystem.b()
