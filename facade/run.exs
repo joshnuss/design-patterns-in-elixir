f9d6b56e1cec34532cce2e17340d1bfa2786a90e
Josh Nussbaum <joshnuss@gmail.com>
2019-06-01 02:07:10 -0400

    Annotates facade pattern

--- facade/run.exs
+++ facade/run.exs
@@ -1,23 +1,29 @@
+# A facade is a portal into a sub-system
+# It encapsulated a sub system
+# It hides implementation details from consumers
+# It acts as a contract between subsystems
+
 # Private implementation details
-defmodule MySystem.ComponentA do
-  def a do
-    IO.puts "component a"
+defmodule Shipping.Packages do
+  def options do
+    [envelope: 1, box: 2]
   end
 end
 
 # More private implementation details
-defmodule MySystem.ComponentB do
-  def b do
-    IO.puts "component b"
+defmodule Shipping.Rates do
+  def compute do
+    [fedex: 1, ups: 2, usps: 0, dhl: 1]
   end
 end
 
-# Facade delegate to underlying components
-defmodule MySystem do
-  defdelegate a, to: MySystem.ComponentA
-  defdelegate b, to: MySystem.ComponentB
+# Facade delegate to underlying components, that means consumers
+# don't need to know about the underlying components
+defmodule Shipping do
+  defdelegate package_options, to: Shipping.Packages, as: :options
+  defdelegate compute_rates, to: Shipping.Rates, as: :compute
 end
 
-# Call facade
-MySystem.a()
-MySystem.b()
+# Call facade and print it
+Shipping.package_options() |> IO.inspect(label: "Packages")
+Shipping.compute_rates() |> IO.inspect(label: "Rates")
